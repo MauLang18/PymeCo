05d7e8e7c216e410ed523684cbc755b0bfbb40ee
mhlebens <marifermatah@gmail.com>
2025-12-08 23:51:41 -0600

    Add InitialCreate migration and configure PymeBD database

--- POS.Infrastructure/Migrations/20251209041220_InitialCreate.cs
+++ POS.Infrastructure/Migrations/20251209041220_InitialCreate.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace POS.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class AddNewDb : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
